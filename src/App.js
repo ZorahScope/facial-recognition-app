cc713e936ba9b14e4a8e810b005b942220407915
Eli <mojojoflow@pm.me>
2022-10-09 20:10:08 -0500

    Add Navigation Componenet
    Built out the skeleton of the navigation componenet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
+import React, { Component } from "react";
+import Navigation from "./components/Navigation/Navigation";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-    
-   </div>
-  );
+// prettier-ignore
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Navigation />
+                {/* <Logo />
+                <ImageLinkForm />
+                <FaceRecognition /> */}
+            </div>
+        );
+    }
 }
 
 export default App;
