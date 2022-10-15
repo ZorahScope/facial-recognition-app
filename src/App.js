34f9f97590fb4c094d0843e27e0de4cc3e69a390
Eli <mojojoflow@pm.me>
2022-10-15 18:13:53 -0500

    Add Rank and ParticlesBg component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import ParticlesBg from "particles-bg";
 import "./App.css";
 
 // prettier-ignore
@@ -9,8 +11,11 @@ class App extends Component {
     render() {
         return (
             <div className="App">
+                <ParticlesBg type="cobweb" bg={true} num={400} />
                 <Navigation />
                 <Logo />
+                <Rank /> 
+
                 <ImageLinkForm />
                 {/* 
                 <FaceRecognition /> */}
